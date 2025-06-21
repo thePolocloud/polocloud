b51f20d9d3714be6110a321a9a9c13ab9e69550d
httpmarco <httpmarco@gmx.de>
2025-06-21 02:05:50 +0200

    Add TextArgument class for handling string command arguments

---
+++ agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/type/TextArgument.kt
@@ -0,0 +1,10 @@
+package dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.type
+
+import dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.CommandArgument
+
+class TextArgument(key: String) : CommandArgument<String>(key) {
+
+    override fun buildResult(input: String): String {
+        return input
+    }
+}
