9572c2f631c4b2e58f9dbcfc43513adabfffb76c
HttpMarco <mirco.lindenau@gmx.de>
2025-12-30 09:56:21 +0100

    chore: add Maven publication configuration for common module in build.gradle.kts and update nexus-deployment.yml

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -20,3 +20,35 @@ kotlin {
 tasks.jar {
     archiveFileName.set("polocloud-common-$version.jar")
 }
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifact(tasks.jar.get())
+
+            pom {
+                description.set("PoloCloud gRPC API with bundled dependencies")
+                url.set("https://github.com/thePolocloud/polocloud")
+
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Mirco Lindenau")
+                        email.set("mirco.lindenau@gmx.de")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/thePolocloud/polocloud")
+                    connection.set("scm:git:https://github.com/thePolocloud/polocloud.git")
+                    developerConnection.set("scm:git:https://github.com/thePolocloud/polocloud.git")
+                }
+            }
+        }
+    }
+}
