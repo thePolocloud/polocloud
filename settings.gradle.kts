0805280e70d13ae4f7a93324e77e5cf98a1ee625
Mirco <mirco@bytemc.de>
2026-01-07 09:23:01 +0100

    Remove runtime modules and related Gradle configuration
    Eliminated `docker-runtime`, `k8s-runtime`, and `local-runtime` modules along with their corresponding build scripts and settings.gradle.kts entries. Simplified project structure by dropping unused runtime components.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,10 +13,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "polocloudv3"
 include("launcher")
-include("runtime")
-include("runtime:k8s-runtime")
-include("runtime:docker-runtime")
-include("runtime:local-runtime")
 include("frames")
 include("frames:frame-gradle-plugin")
 include("frames:frame-api")
+include("cli")
