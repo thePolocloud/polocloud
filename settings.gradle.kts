481b494d213886e631ed94c9d219d3f694c161b5
httpmarco <httpmarco@gmx.de>
2026-01-09 22:59:09 +0100

    feat: add 'proto' module to project includes in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,4 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "polocloudv3"
 include("frames", "frames:frame-gradle-plugin", "frames:frame-api", "cli", "runner")
+include("proto")
