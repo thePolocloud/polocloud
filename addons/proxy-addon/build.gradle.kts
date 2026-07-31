a8b80a0aba3203025474e6367bdf2a6d82a95c35
httpmarco <httpmarco@gmx.de>
2026-07-31 23:04:53 +0200

    feat: update settings.gradle.kts to include proxy-addon and server-mobs-addon; add build.gradle.kts for project configuration

---
+++ addons/proxy-addon/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "de.polocloud"
+version = "3.0.0-snapshot.local"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
