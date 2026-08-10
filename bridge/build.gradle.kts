c5efc34199f0e18699cdb96c2d862a54459219e2
NobleMajo <39386799+NobleMajo@users.noreply.github.com>
2026-08-11 01:05:12 +0200

    revert 0d1f369

--- bridge/build.gradle.kts
+++ bridge/build.gradle.kts
@@ -42,7 +42,7 @@ kotlin {
 tasks.processResources {
     val tokens = mapOf("version" to project.version.toString())
     inputs.properties(tokens)
-    filesMatching(listOf("velocity-plugin.json")) {
+    filesMatching(listOf("velocity-plugin.json", "bungee.yml")) {
         expand(tokens)
     }
 }
